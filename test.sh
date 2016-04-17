c1153557689817fa828975615be1c4db2cccfa99
toshihirohayashi <hayasyun777@yahoo.co.jp>
2016-04-17 17:21:07 +0200

    testdata

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ cd ./testdata/
 #nucmer -maxmatch -c 100 -p abcbd abcbd.fa abcbd.fa
 #nucmer -maxmatch -c 100 -p abd_or_acd abd_or_acd.fa abd_or_acd.fa    
 
-python3 ../olcAssembler -i abcd.delta -f abcd.fa -o assembly_abcd.fa -c ON
-python3 ../olcAssembler -i abcbd.delta -f abcbd.fa -o assembly_abcbd.fa -c ON
-python3 ../olcAssembler -i abd_or_acd.delta -f abd_or_acd.fa -o assembly_abd_or_acd.fa -c ON
+python3 ../olcAssembler -i contigs/abcd.delta -f contigs/abcd.fa -o assemblies/assembly_abcd.fa -c ON
+python3 ../olcAssembler -i contigs/abcbd.delta -f contigs/abcbd.fa -o assemblies/assembly_abcbd.fa -c ON
+python3 ../olcAssembler -i contigs/abd_or_acd.delta -f contigs/abd_or_acd.fa -o assemblies/assembly_abd_or_acd.fa -c ON
